1663efd5c2d370b29673c7aa90d164178ceb1868
Yoel Rodriguez <yo3l.m18@gmail.com>
2020-06-13 16:14:34 -0500

    init wordpress

---
+++ Wordpress/revert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export namespace='wordpress01'
+
+kubectl config set-context --current --namespace=$namespace
+kubectl config view | grep namespace:
+
+kubectl delete -f memcached.deployment.yaml --namespace=$namespace
+kubectl delete -f memcached.service.yaml --namespace=$namespace
+
+kubectl delete -f wordpress.deployment.yaml --namespace=$namespace
+kubectl delete -f wordpress.service.yaml --namespace=$namespace
+
+kubectl delete -f mysql.deployment.yaml --namespace=$namespace
+kubectl delete -f mysql.service.yaml --namespace=$namespace
+
+kubectl delete secret wp01.mysql.secret --namespace=$namespace
+
+kubectl delete -f mysql.pvc.yaml --namespace=$namespace
+kubectl delete -f system.pvc.yaml --namespace=$namespace
+
+kubectl delete -f wp01.storageclass.yaml --namespace=$namespace
+
+kubectl delete -f namespace.yaml
