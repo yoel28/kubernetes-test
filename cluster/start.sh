19cf160fd69172d6ec7f1d259b335382ca2d4ed5
Yoel Rodriguez <yo3l.m18@gmail.com>
2020-06-21 19:03:51 -0500

    test

--- cluster/start.sh
+++ cluster/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
+export AWS_PROFILE=adminCli
+export KUBE_EDITOR="nano"
 eksctl create cluster -f eks-dev.cluster.yaml
+
+#eksctl delete nodegroup -f eks-dev.cluster.yaml --exclude=ng-bs --approve
+#eksctl create nodegroup -f eks-dev.cluster.yaml --include=ng-ux
+#eksctl scale nodegroup --cluster=eks-dev --name=ng-ux --nodes=1
+
+#kubectl get pods -n kube-system
+#kubectl -n kube-system logs -f deployment.apps/cluster-autoscaler
